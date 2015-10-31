52d436b2a2c15e40d7e4224307942c71cb5933dd
mohitbadaun <mohitbadaun@gmail.com>
2015-10-31 18:57:52 +0530

    added library

---
+++ library/app/src/main/java/com/semicolon/findhouse/MainActivity.java
@@ -0,0 +1,26 @@
+package com.semicolon.findhouse;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tv = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tv = (TextView) findViewById(R.id.txt_status);
+        TBButton btn = (TBButton) findViewById(R.id.btn_submit);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tv.setText("Clicked captured");
+            }
+        });
+    }
+}
